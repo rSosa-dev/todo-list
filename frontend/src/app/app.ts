e5236514de36558afe89a4efedb9a0e3c47a9787
Raúl Sosa <RaulSG188@gmail.com>
2026-05-11 11:05:03 +0200

    Component added

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeComponent } from "./welcome.component/welcome.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WelcomeComponent],
   templateUrl: './app.html',
+  //template: '<h1>{{ title() }}</h1>',
   styleUrl: './app.css'
 })
 export class App {
