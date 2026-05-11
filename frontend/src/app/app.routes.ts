0b498989b9839c8ffec754f89554af69ef4fa168
Raúl Sosa <RaulSG188@gmail.com>
2026-05-11 19:34:37 +0200

    Init routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login.component/login.component';
+import { WelcomeComponent } from './welcome.component/welcome.component';
+import { ErrorComponent } from './error.component/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'welcome', component: WelcomeComponent},
+    { path: '**', component: ErrorComponent}
+];
--- frontend/src/app/login.component/login.component.ts
+++ frontend/src/app/login.component/login.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  username = 'Raúl Sosa'
+  password = ''
+  errorMessage = 'Invalid credentials'
+  invalidLogin = false
+
+  handleLogin() {
+    // console.log(this.username);
+
+    if(this.username === "Raúl Sosa" && this.password === "dummy") {
+      this.invalidLogin = false
+    } else {
+      this.invalidLogin = true
+    }
+  }
+}
