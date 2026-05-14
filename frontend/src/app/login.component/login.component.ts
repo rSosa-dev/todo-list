1b86a3adb78a37d19506f1417c5d614c7416605e
Raúl Sosa <raul.sosa@boehringer-ingelheim.com>
2026-05-14 14:18:57 +0200

    Added routing

--- frontend/src/app/login.component/login.component.ts
+++ frontend/src/app/login.component/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,10 +16,15 @@ export class LoginComponent {
   errorMessage = 'Invalid credentials'
   invalidLogin = false
 
+  constructor(private router: Router) { }
+
   handleLogin() {
     // console.log(this.username);
 
     if(this.username === "Raúl Sosa" && this.password === "dummy") {
+      // Redirect to welcome page
+      this.router.navigate(['welcome', this.username])
+
       this.invalidLogin = false
     } else {
       this.invalidLogin = true
--- frontend/src/app/welcome.component/welcome.component.ts
+++ frontend/src/app/welcome.component/welcome.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
@@ -6,4 +8,14 @@ import { Component } from '@angular/core';
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css',
 })
-export class WelcomeComponent {}
+export class WelcomeComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) { }
+
+  name = ''
+
+  ngOnInit() {
+    console.log(this.route.snapshot.params['name']);
+    this.name = this.route.snapshot.params['name'];
+  }
+}
