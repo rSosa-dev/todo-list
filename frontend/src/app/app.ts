5ba6b71e3b9394f3fe086f5bf4849f32c3cb274d
Raúl Sosa <raul.sosa@boehringer-ingelheim.com>
2026-05-15 08:15:41 +0200

    Styling

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,14 +2,17 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from "./welcome.component/welcome.component";
 import { LoginComponent } from './login.component/login.component';
+import { ListTodosComponent } from './list-todos.component/list-todos.component';
+import { MenuComponent } from './menu.component/menu.component';
+import { FooterComponent } from './footer.component/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WelcomeComponent, LoginComponent],
+  imports: [RouterOutlet, WelcomeComponent, LoginComponent, ListTodosComponent, MenuComponent, FooterComponent],
   templateUrl: './app.html',
   //template: '<h1>{{ title() }}</h1>',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal('Todo');
 }
